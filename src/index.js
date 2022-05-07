517d6a64862e4f6d25a9ab89ccedd164ad860844
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-05-07 23:00:01 +0530

    Added signup page, updated homepage

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from 'store';
+import { ErrorBoundary } from 'components';
 import App from 'pages/App';
 import reportWebVitals from './reportWebVitals';
 import 'styles/index.css';
@@ -9,9 +10,11 @@ import 'styles/index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
 
