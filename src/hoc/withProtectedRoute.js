474a7f058d8b1ee2dd18c943990be911791a5cca
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-05-08 15:18:37 +0530

    Updated route hocs

--- src/hoc/withProtectedRoute.js
+++ src/hoc/withProtectedRoute.js
@@ -2,18 +2,18 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectAuthUser } from 'store/selectors';
 
-const withAuthRoute = (Component) => {
+const withProtectedRoute = (Component) => {
   return (props) => {
     const { state } = useLocation();
     const user = useSelector(selectAuthUser);
 
-    const pathname = state?.from?.pathname || '/';
+    const pathname = state?.from?.pathname || '/signin';
 
-    if (user) {
+    if (!user) {
       return <Navigate to={pathname} />;
     }
     return <Component {...props} />;
   };
 };
 
-export default withAuthRoute;
+export default withProtectedRoute;
