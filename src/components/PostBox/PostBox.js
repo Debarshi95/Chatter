ce1e53579097f3edba578d2d9be1a09d707f3f11
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-05-27 14:15:11 +0530

    Updated PostBox CardHeader PostBoxFooter

--- src/components/PostBox/PostBox.js
+++ src/components/PostBox/PostBox.js
@@ -1,10 +1,7 @@
 import sanitizeHtml from 'sanitize-html';
-import cn from 'clsx';
 import { useRef } from 'react';
-import { BiComment } from 'react-icons/bi';
-import { MdOutlineDelete } from 'react-icons/md';
+import { MdDelete } from 'react-icons/md';
 import { CardHeader, Button, PostBoxFooter } from 'components';
-import { Link } from 'react-router-dom';
 
 const PostBox = ({
   contentEditable,
@@ -34,10 +31,10 @@ const PostBox = ({
   };
 
   return (
-    <div className="bg-slate-700 rounded-lg p-4 mb-4">
+    <div className="bg-slate-800 border border-gray-700 rounded-lg p-2 mb-2">
       {headerComponent || (
         <CardHeader
-          avatarClassName="w-20 h-20"
+          avatarClassName="w-14 h-14"
           avatar={post?.user?.avatar}
           userId={post?.user?.id}
           username={post?.user?.username}
@@ -50,36 +47,22 @@ const PostBox = ({
           ref={contentRef}
           contentEditable={contentEditable}
           dangerouslySetInnerHTML={{ __html: sanitizeHtml(post?.content || post?.text) }}
-          className="bg-transparent my-6 text-lg outline-none w-full h-full text-gray-300"
+          className="bg-transparent my-4 text-base outline-none w-full h-full text-slate-300"
           data-placeholder={placeholder}
         />
         {contentEditable && (
           <Button
-            className="w-32 rounded-lg p-2 bg-slate-500 text-base text-white ml-auto"
+            className="w-32 rounded-lg p-2 bg-slate-700 text-base text-white ml-auto"
             onClick={handlePostClick}
           >
             Post
           </Button>
         )}
         {showPostIcons && (
-          <div className="flex text-slate-300 text-2xl cursor-pointer">
-            <Link to={`/comment/${post?.id}`} className="flex">
-              <BiComment
-                className={
-                  (cn('block mr-auto hover:text-slate-500 '),
-                  {
-                    'mr-10': canDeletePost,
-                  })
-                }
-              />
-              <p className="text-base ml-2">{post?.comments?.length}</p>
-            </Link>
+          <div className="flex content-between text-slate-300 text-2xl cursor-pointer py-3 border-stone-500 border-t">
             <PostBoxFooter post={post} onUpdate={onUpdatePost} />
             {canDeletePost && (
-              <MdOutlineDelete
-                className="text-2xl block ml-4 md:ml-20 hover:text-white"
-                onClick={() => onDeletePost(post.id)}
-              />
+              <MdDelete className="block font-thin" onClick={() => onDeletePost(post.id)} />
             )}
           </div>
         )}
